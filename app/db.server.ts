4237d2aa79694fe0d0bd1fb4642adb36c8e1023e
Anurag <anuragyadav20062002@gmail.com>
2025-06-25 23:50:15 +0530

    exited

--- app/db.server.ts
+++ app/db.server.ts
@@ -26,21 +26,6 @@ if (process.env.NODE_ENV === 'production') {
   prisma = global.__prisma
 }
 
-prisma.$use(async (params, next) => {
-  try {
-    return await next(params)
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error('Database error:', {
-        model: params.model,
-        action: params.action,
-        error: error.message,
-      })
-    }
-    throw error
-  }
-})
-
 export { prisma }
 export default prisma
 
